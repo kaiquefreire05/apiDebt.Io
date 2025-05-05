651aa16d1b8b09e48b6223dbc7e7481a08c6eeb1
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2025-05-05 17:16:41 -0300

    feat: rest use case

---
+++ src/main/java/com/v1/apiDebt/Io/application/ports/input/usuario/CriarUsuarioUseCase.java
@@ -0,0 +1,7 @@
+package com.v1.apiDebt.Io.application.ports.input.usuario;
+
+import com.v1.apiDebt.Io.domain.models.Usuario;
+
+public interface CriarUsuarioUseCase {
+    Usuario criar(Usuario usuario);
+}
