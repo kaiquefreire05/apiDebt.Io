616ed6d3d02ac4ce34c94dff7661bdf64a05a2fb
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2025-05-06 22:06:13 -0300

    feat: others setup files and log file config

---
+++ src/main/java/com/v1/apiDebt/Io/Application.java
@@ -0,0 +1,13 @@
+package com.v1.apiDebt.Io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
