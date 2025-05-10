5082c5f717284be6bdc92609017f0dd93634f232
Kaíque Freire dos Santos <kaiquefreiresantos05@gmail.com>
2025-05-10 13:42:14 -0300

    feat: adding console logger

---
+++ src/main/java/com/v1/apiDebt/Io/infra/util/Utils.java
@@ -0,0 +1,8 @@
+package com.v1.apiDebt.Io.infra.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Utils {
+    public static Logger logConsole = LoggerFactory.getLogger("Debt.IoAPI");
+}
